c2b3b2193a2b13eed77931075420d7b16fe412cc
h-tin <45798183+h-tin@users.noreply.github.com>
2021-02-20 22:07:47 +0900

    Add files via upload

--- cprest/client.py
+++ cprest/client.py
@@ -23,24 +23,27 @@ urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class Client:
-    """ClearPass RESTful API Client."""
+    """ClearPass RESTful API Client.
 
-    def __init__(self, host: str = "", port: int = 443,
-                 timeout: Union[Tuple[float, float], float] = 30.0,
-                 verify_cert: bool = False):
-        """Constructor.
+    Attributes:
+        host (str): API Server hostname or IP address, default empty.
+        port (int): API Server port number between 1 and 65535, default 443.
+        timeout (Union[float, Tuple[float, float]]): Timeout seconds to wait for
+            server connection and server response, default 30.0. Tuples set them
+            different values.
+        verify_cert (bool): Enable SSL certificate validation, default False.
+    """
+
+    def __init__(self, **kwargs):
+        """Initializes Client.
 
         Args:
-            host: API Server hostname or IP address.
-            port: API Server listening port number between 1 and 65535.
-            timeout: Timeout seconds to wait for server connection and server response.
-                     A float value is set to the same value for both.
-            verify_cert: Enable SSL certificate validation.
+            **kwargs: Arbitrary keyword arguments that initializes attributes.
         """
-        self.host = host
-        self.port = port
-        self.timeout = timeout
-        self.verify_cert = verify_cert
+        self.host = kwargs["host"] if "host" in kwargs else ""
+        self.port = kwargs["port"] if "port" in kwargs else 443
+        self.timeout = kwargs["timeout"] if "timeout" in kwargs else 30.0
+        self.verify_cert = kwargs["verify_cert"] if "verify_cert" in kwargs else False
         self._authorized_at = None
         self._access_token = None
         self._token_type = None
@@ -49,7 +52,7 @@ class Client:
 
     @property
     def is_authorized(self) -> bool:
-        """Whether the client is authorized (expiration is not considered)."""
+        """Whether the client is authorized. Expiration is not considered."""
         return True if self._access_token else False
 
     @property
@@ -83,24 +86,29 @@ class Client:
         else:
             return "".join(["https://", self.host, ":", str(self.port), "/api"])
 
-    def request(self, method: str, resource: str,
-                params={}, body={}) -> requests.Response:
-        """Invoke an API requerst.
+    def request(self, method: str, resource: str, **kwargs) -> requests.Response:
+        """Invoke an API request.
 
         Args:
-            method: HTTP request method. One of GET, POST, PATCH, PUT, DELETE.
-            resource: Target resource.
-            params: Query parameters added to the URL, e.g. ?key1=value1&key2=value2&...
-            body: Request body sent as JSON, e.g. {"key1":"value1","key2":"value2",...}
+            method (str): One of GET, POST, PATCH, PUT, DELETE.
+            resource (str): Target resource.
+
+        Keyword Args:
+            params (dict): Query parameters added to the URL, default empty.
+                {"key1"":"value1","key2":"value2"} -> ?key1=value1&key2=value2
+            body (dict): Request body sent as JSON, default empty.
 
         Raises:
-            RequestException: There was an ambiguous exception that occurred while handling your request.
+            RequestException: There was an ambiguous exception that occurred while
+                handling your request.
             ConnectionError: A Connection error occurred.
             HTTPError: An HTTP error occurred.
             URLRequired: A valid URL is required to make a request.
             TooManyRedirects: Too many redirects.
-            ConnectTimeout: The request timed out while trying to connect to the remote server.
-            ReadTimeout: The server did not send any data in the allotted amount of time.
+            ConnectTimeout: The request timed out while trying to connect to the
+                remote server.
+            ReadTimeout: The server did not send any data in the allotted amount
+                of time.
             Timeout: The request timed out.
             ValueError: One or more invalid arguments were passed.
 
@@ -114,39 +122,38 @@ class Client:
         headers = {}
         if self.is_authorized:
             headers["Authorization"] = self.authorization_header
-        if len(body) > 0:
+        if "body" in kwargs and len(kwargs["body"]) > 0:
             headers["Content-Type"] = "application/json"
 
         # Invoke request.
         return requests.request(
-            method=method, url=self.base_url + resource,
-            params=params, headers=headers, json=body,
-            timeout=self.timeout, verify=self.verify_cert)
+            method=method,
+            url=self.base_url + resource,
+            params=kwargs["params"] if "params" in kwargs else {},
+            headers=headers,
+            json=kwargs["body"] if "body" in kwargs else {},
+            timeout=self.timeout,
+            verify=self.verify_cert)
 
-    def get(self, resource: str, params={}) -> requests.Response:
+    def get(self, resource: str, **kwargs) -> requests.Response:
         """Call self.request() with GET method."""
-        return self.request(
-            method="GET", resource=resource, params=params)
+        return self.request(method="GET", resource=resource, **kwargs)
 
-    def post(self, resource: str, params={}, body={}) -> requests.Response:
+    def post(self, resource: str, **kwargs) -> requests.Response:
         """Call self.request() with POST method."""
-        return self.request(
-            method="POST", resource=resource, params=params, body=body)
+        return self.request(method="POST", resource=resource, **kwargs)
 
-    def patch(self, resource: str, params={}, body={}) -> requests.Response:
+    def patch(self, resource: str, **kwargs) -> requests.Response:
         """Call self.request() with PATCH method."""
-        return self.request(
-            method="PATCH", resource=resource, params=params, body=body)
+        return self.request(method="PATCH", resource=resource, **kwargs)
 
-    def put(self, resource: str, params={}, body={}) -> requests.Response:
+    def put(self, resource: str, **kwargs) -> requests.Response:
         """Call self.request() with PUT method."""
-        return self.request(
-            method="PUT", resource=resource, params=params, body=body)
+        return self.request(method="PUT", resource=resource, **kwargs)
 
-    def delete(self, resource: str, params={}) -> requests.Response:
+    def delete(self, resource: str, **kwargs) -> requests.Response:
         """Call self.request() with DELETE method."""
-        return self.request(
-            method="DELETE", resource=resource, params=params)
+        return self.request(method="DELETE", resource=resource, **kwargs)
 
     def authenticate(self, grant_type: str, client_id: str,
                      client_secret: str = "",
@@ -163,15 +170,7 @@ class Client:
             refresh_token (str): Refresh token for refresh token grant type.
 
         Raises:
-            RequestException: There was an ambiguous exception that occurred while handling your request.
-            ConnectionError: A Connection error occurred.
-            HTTPError: An HTTP error occurred.
-            URLRequired: A valid URL is required to make a request.
-            TooManyRedirects: Too many redirects.
-            ConnectTimeout: The request timed out while trying to connect to the remote server.
-            ReadTimeout: The server did not send any data in the allotted amount of time.
-            Timeout: The request timed out.
-            ValueError: One or more invalid arguments were passed.
+            Same as self.request() method.
 
         Returns:
             None means no error.
